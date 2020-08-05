bf56010964a94b1335f2a2bcabbde4cc77928554
divinosdev <vivek99.work@gmail.com>
2020-08-05 23:08:45 +0530

    basic tree implementation

--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
-function setup()
-{
+var angle = 0;
+var divideValue = 0.7;
 
+function setup() {
+    createCanvas(500, 500);
+    slider = createSlider(0, TWO_PI, PI / 4, 0.001);
 }
 
-function draw()
-{
+function draw() {
+    background(51);
+    angle = slider.value();
+    stroke(255);
+    translate(250, height);
+    branch(100);
+}
 
+function branch(len) {
+    line(0, 0, 0, - len);
+    translate(0, -len);
+    if (len > 4) {
+        push();
+        rotate(angle);
+        branch(len * divideValue)
+        pop();
+        push();
+        rotate(-angle);
+        branch(len * divideValue);
+        pop();
+    }
 }
