bf3a5c57398b3c5af2b8197e38a1361373b7566c
divinosdev <vivek99.work@gmail.com>
2020-08-08 00:16:04 +0530

    twinkling stars

--- sketch.js
+++ sketch.js
@@ -4,13 +4,17 @@ var counter = 0;
 
 var starX = [];
 var starY = [];
+var stars = [];
 
 
 function setup() {
 
     createCanvas(windowWidth, windowHeight);
 
-    getStarPositions();
+    //create stars
+    for (var i = 0; i < 50; i++) {
+        stars[i] = new Star();
+    }
 
     var a = createVector(width / 2, height);
     var b = createVector(width / 2, height - 100);
@@ -50,10 +54,7 @@ function createForest() {
 
 function draw() {
 
-    //Gradient Show
-    var color1 = color(7, 11, 52);  //top
-    var color2 = color(133, 89, 136); //bottom
-    setGradient(0, 0, windowWidth, windowHeight, color1, color2, "Y");
+    drawGradient();
 
     drawTree();
 
@@ -78,24 +79,29 @@ function drawTree() {
 }
 
 
-function getStarPositions() {
-    for (let i = 0; i < 70; i++) {
-        starX[i] = random(windowWidth);
-        starY[i] = random(windowHeight - 250);
-    }
-}
-
 function drawStars() {
 
-    for (let i = 0; i < 70; i++) {
-        noStroke();
-        fill(255, 255, 255);
-        ellipse(starX[i], starY[i], 2, 2);
+    for (var i = 0; i < stars.length; i++) {
+        stars[i].draw();
     }
-
+    //moon
     noStroke();
+    fill(230, 230, 230, 50);
+    ellipse(200, 150, 170, 170);
+    fill(230, 230, 230, 150);
+    ellipse(200, 150, 130, 130);
+    fill(230, 230, 230, 200);
+    ellipse(200, 150, 110, 110);
     fill(230, 230, 230);
-    ellipse(200, 150, 90, 90);
+    ellipse(200, 150, 70, 70);
+}
+
+//Show Gradient
+function drawGradient() {
+    var color1 = color(7, 11, 52);  //top
+    var color2 = color(133, 89, 136); //bottom
+    setGradient(0, 0, windowWidth, windowHeight, color1, color2, "Y");
+
 }
 
 
@@ -109,5 +115,24 @@ function setGradient(x, y, w, h, c1, c2, axis) {
             stroke(c);
             line(x, i, x + w, i);
         }
+    }
+}
+
+// Stars //
+class Star {
+    constructor() {
+        this.x = random(windowWidth);
+        this.y = random(windowHeight - 250);
+        this.size = random(0.25, 4);
+        this.t = random(TAU);
+    }
+
+    draw() {
+        this.t += 0.1;
+        var scale = this.size + sin(this.t) * 0.8;
+        noStroke();
+        fill(255, 255, 255);
+
+        ellipse(this.x, this.y, scale, scale);
     }
 }
